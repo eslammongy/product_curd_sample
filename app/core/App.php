ce348055e4ee94ba9005312195a184b437433820
eslam <eslammongy995@gmail.com>
2022-01-12 19:55:13 +0200

    adding home controller

--- app/core/App.php
+++ app/core/App.php
@@ -7,14 +7,35 @@ class  App{
 
     public  function __construct()
     {
+        $this->preparingUrl();
+        $this->render();
+    }
+
+    // extract controller and method and parameters and return void
+    private function preparingUrl(){
         $url = $_SERVER["QUERY_STRING"];
 
         if (!empty($url)){
+            $url = trim($url , '/');
             $url = explode("/" , $url);
-            echo "<b></b> <h1>$url[1]</h1>";
+            $this->controller = isset($url[0]) ? ucwords($url[0])."Controller": "HomeController";
+            $this->action = $url[1] ?? "index";
+            unset($url[0] , $url[1]);
+            $this->parms = !empty($url) ? array_values($url) : [];
+            var_dump($this->parms);
+            echo "<h1 style='text-align: center ; color: orangered'>$this->controller</h1>";
         }else{
             echo "<b></b> <h1 style='text-align: center ; color: orangered'>Welcome</h1>";
         }
+    }
 
+    private function render(){
+        if (class_exists($this->controller)){
+            $message = "Hello From Home Controller";
+            echo "<h1 style='text-align: center ; color: orangered'>$message</h1>";
+        }else{
+            $message = "This Controller :".$this->controller. " Not Exit";
+            echo "<h1 style='text-align: center ; color: orangered'>$message</h1>";
+        }
     }
 }
